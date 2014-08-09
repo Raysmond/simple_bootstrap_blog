1b1e0a7580ec3b319325d837178ac9103c357c22
Raysmond <jiankunlei@126.com>
2014-08-09 21:50:13 +0800

    U

--- js/local.js
+++ js/local.js
@@ -2,28 +2,29 @@
  * Local script file.
  * @author: Raysmond
  */
-jQuery(document).ready(function($){
-	var flag = true;
-    $(".content .field-name-body img").css("height","auto");
+jQuery(document).ready(function($) {
+    var flag = true;
+    $(".content .field-name-body img").css("height", "auto");
     jQuery("a#controlbtn").click(function(e) {
         e.preventDefault();
-        var slidepx= flag ? 260 : 0;
-    	// if ( !jQuery("#col8").is(':animated') ) { 
-    		flag = !flag;
-    		var w = "" + (980 - slidepx) + "px";
-        	$("#col8").animate({ 
-        		width: w
-      		}, {
-                    duration: 'slow',
-                    easing: 'easeOutQuint'
-                });
-        	$('#col4').css("display", slidepx == 0 ? "none" : "block");
-        	$("#col4").animate({ 
-        		width: ""+slidepx+"px"
-      		}, {
-                    duration: 'slow',
-                    easing: 'easeOutQuint'
-                });
-    	// } 
-      }); 
+        var slidepx = flag ? 260 : 0;
+        if (!jQuery("#col8").is(':animated')) {
+            flag = !flag;
+            $("#col8").animate({
+                width: "" + (980 - slidepx) + "px"
+            },
+            {
+                duration: 'slow',
+                easing: 'easeOutQuint'
+            });
+            $('#col4').css("display", slidepx == 0 ? "none": "block");
+            $("#col4").animate({
+                width: "" + slidepx + "px"
+            },
+            {
+                duration: 'slow',
+                easing: 'easeOutQuint'
+            });
+        }
+    });
 });
